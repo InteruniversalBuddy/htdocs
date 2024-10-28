ed069910b5d10a59390ae29f4670e4f60e0c855f
InteruniversalBuddy <InteruniversalBuddy@gmail.com>
2024-10-28 23:06:25 +0100

    fridge projekt

--- Tag5/app/fridge/fridge.php
+++ Tag5/app/fridge/fridge.php
@@ -2,10 +2,25 @@
 
 namespace app\fridge;
 
-class Fridge
+/**
+ * Class fridge
+ *
+ * A simple fridge class to store items.
+ */
+class fridge
 {
-    public function __construct()
+    /**
+     * @var array List of items in the fridge
+     */
+    public array $items = [];
+
+    /**
+     * Fridge constructor.
+     *
+     * @param array $items Initial items in the fridge
+     */
+    public function __construct(array $items = [])
     {
-        
+        $this->items = $items;
     }
 }
---
+++ Tag5/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'interuniversalbuddy/tag5',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '03e4c49bfaf904da0ebc60804215a88ac40382ac',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'interuniversalbuddy/tag5' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '03e4c49bfaf904da0ebc60804215a88ac40382ac',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'steampixel/simple-php-router' => array(
+            'pretty_version' => '0.7.1',
+            'version' => '0.7.1.0',
+            'reference' => 'c996d75b34c2c6da45d4dc737f61c0ee8fed5d2b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../steampixel/simple-php-router',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
